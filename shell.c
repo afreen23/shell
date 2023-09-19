3e1da8e4309249551b08b7a346be8cd92c370b41
Afreen <25664409+afreen23@users.noreply.github.com>
2023-09-19 12:09:59 +0530

    Feat: Added exit command
    - takes no arguments
    - returns error if argument provided
    - exits with status 0
    
    Signed-off-by: Afreen <25664409+afreen23@users.noreply.github.com>

--- shell.c
+++ shell.c
@@ -2,37 +2,101 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<ctype.h>
+#include<string.h>
+
+
+#define DIR_CHARACTERS 128
+#define INPUT_CHARACTERS 128
+#define ARGUMENTS 50
+
+/* Current working directory path */
+char path[DIR_CHARACTERS];
+
+void prompt() {
+	getcwd(path, DIR_CHARACTERS);
+	printf("%s$ ", path);		
+}
+
+void exit_command(char (*args)[], int size) {
+	if(size != 0) {
+		printf("Invalid argument: exit takes no arguments.\n");
+		return;
+	}
+	exit(0);
+}
+
+// void cd_command(char** args, int size) {
+// 	if (size != 1) {
+// 		printf("Usage: cd takes only one argument\n");
+// 		return;
+// 	}
+
+
+// }
+
+// void exec_command(char** args, int size) {
+// 	if(cmds == NULL) {
+// 		printf("Usage: \n");
+// 		return;
+// 	}
+
+// }
 
 int main() {
-	// getting the current working directory
-	char path[128];
-	getcwd(path, 128);
-	printf("%s$ ", path);
-	// reading lines of user input
+	prompt();
+
 	char* line;
-	size_t size = 30;
+	size_t size = INPUT_CHARACTERS;
 	int characters;
 	line = (char *)malloc(sizeof(char)*size);
 	if(line == NULL) {
 		perror("Unable to allocate buffer\n");
 		exit(1);
 	}
-	
+
 	while(1) {
-		characters =getline(&line, &size, stdin);
-		if(characters == -1) {
+		if(getline(&line, &size, stdin) == -1) {
 			exit(1);
 		}
 		if(line[0] != '\n') {
-			int i = 0, k = 0;
-			char word[128];
-			while(k < 128 && line[i] != '\0' && !isspace(line[i])){
-				word[k++] = line[i++];
+			/* Input command */
+			int l = 0, k = 0;
+			char cmd[INPUT_CHARACTERS];
+			while(k < INPUT_CHARACTERS && line[l] != '\0' && !isspace(line[l])){
+				cmd[k++] = line[l++];
 			}
-			word[k]='\0';
-			printf("Unrecognized command: %s\n", word);
+			if(line[l] == '\0') {
+				cmd[k] = l;
+			}
+			else {
+				cmd[k] = '\0';
+				l++;
+			}
+			/* Arguments of command */
+			char args[ARGUMENTS][INPUT_CHARACTERS];
+			int i = 0,  j = 0;
+			while(i < ARGUMENTS && j < INPUT_CHARACTERS && line[l] != '\0'){
+				if(isspace(line[l])) {
+					args[i][j] = '\0';
+					i++;
+					l++;
+				}
+				args[i][j++] = line[l++];
+			}	
+			if(strcmp(cmd, "exit") == 0) {
+				exit_command(args, i);
+			}
+			// else if(strcomp(cmd, "cd") == 0) {
+			// 	cd_command(args, i);
+			// }
+			// else if(strcomp(cmd. "exec") == 0) {
+			// 	exec_command(args, i);
+			// }
+			else {
+				printf("Unrecognized command: %s\n", cmd);
+			}
 		}
-		printf("%s$ ", path);		
+		prompt();
 	}
 
 	return 0;
