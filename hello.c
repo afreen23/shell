a9564148e4b01c2b4f69f1da9241c455be6f0caa
Afreen <25664409+afreen23@users.noreply.github.com>
2023-09-19 14:02:01 +0530

    Feat: Added exec command
    - replaces the shell with the specified command
    
    Signed-off-by: Afreen <25664409+afreen23@users.noreply.github.com>

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+main(int argc,char *argv[],char *envp[]){
+printf("Filename: %s\n",argv[0]);
+printf("%s %s\n",argv[1],argv[2]);
+}
--- shell.c
+++ shell.c
@@ -4,7 +4,6 @@
 #include<ctype.h>
 #include<string.h>
 
-
 #define DIR_CHARACTERS 128
 #define INPUT_CHARACTERS 128
 #define ARGUMENTS 50
@@ -25,24 +24,28 @@ void exit_command(int size) {
 	exit(0);
 }
 
-void cd_command(char (*args)[], int size) {
+void cd_command(char** args, int size) {
 	if (size != 1) {
 		printf("Usage: cd takes only one argument, %d argument provided.\n", size);
 		return;
 	}
 	int errno = chdir(*args);
 	if(errno == -1) {
-		printf("Command failed with error code -1");
+		printf("Command failed with error code -1\n");
 	}
 }
 
-// void exec_command(char** args, int size) {
-// 	if(cmds == NULL) {
-// 		printf("Usage: \n");
-// 		return;
-// 	}
-
-// }
+void exec_command(char** args, int size) {
+	if(size < 1) {
+		printf("Usage: exec takes at least one argument %d arguments provided.\n", size);
+		return;
+	}
+	const char* pathname = args[0];
+	int errno = execv(pathname, args);
+	if(errno == -1) {
+		printf("Command failed with error code -1\n");
+	}
+}
 
 int main() {
 	prompt();
@@ -75,25 +78,30 @@ int main() {
 				l++;
 			}
 			/* Arguments of command */
-			char args[ARGUMENTS][INPUT_CHARACTERS];
+			char** args= (char**)malloc(sizeof(char*)*ARGUMENTS);
 			int i = 0,  j = 0;
+			args[i] = (char*)malloc(sizeof(char)*INPUT_CHARACTERS);
 			while((i < ARGUMENTS) && (j < INPUT_CHARACTERS) && (line[l] != '\0')){
 				if(isspace(line[l])) {
 					args[i][j] = '\0';
 					i++;
+					j=0;
 					l++;
+					args[i] = (char*)malloc(sizeof(char)*INPUT_CHARACTERS);
 				}
-				args[i][j++] = line[l++];
-			}	
+				else
+					args[i][j++] = line[l++];
+			}
+			args[i][j] = '\0';	
 			if(strcmp(cmd, "exit") == 0) {
 				exit_command(i);
 			}
 			else if(strcmp(cmd, "cd") == 0) {
 				cd_command(args, i);
 			}
-			// else if(strcmp(cmd. "exec") == 0) {
-			// 	exec_command(args, i);
-			// }
+			else if(strcmp(cmd, "exec") == 0) {
+				exec_command(args, i);
+			}
 			else {
 				printf("Unrecognized command: %s\n", cmd);
 			}
