e82e8090307de6703b9db2d0c455f65f6a487c6d
Afreen <25664409+afreen23@users.noreply.github.com>
2023-09-23 15:43:14 +0530

    Feat: Allow searching of file and executing it if input did not match
    - added code for freeing up memory
    - added perror to getline
    - changed args to include the command as well in order to make execv work properly
    
    Signed-off-by: Afreen <25664409+afreen23@users.noreply.github.com>

--- shell.c
+++ shell.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<errno.h>
 #include<sys/types.h>
+#include<sys/stat.h>
 #include<unistd.h>
 #include<stdlib.h>
 #include<ctype.h>
@@ -11,17 +12,14 @@
 #define INPUT_CHARACTERS 128
 #define ARGUMENTS 10
 
-/* Current working directory path */
-char path[DIR_CHARACTERS];
-
 void prompt() {
+	char path[DIR_CHARACTERS]; // Current working directory path
 	getcwd(path, DIR_CHARACTERS);
 	printf("%s$ ", path);		
 }
 
-
 void exit_command(int size) {
-	if(size != 0) {
+	if((size-1) != 0) {
 		printf("Invalid argument: exit takes no arguments\n");
 		return;
 	}
@@ -29,11 +27,11 @@ void exit_command(int size) {
 }
 
 void cd_command(char** args, int size) {
-	if (size != 1) {
-		printf("Usage: cd takes only one argument, %d arguments provided\n", size);
+	if ((size-1) != 1) {
+		printf("Usage: cd takes only one argument, %d arguments provided\n", size-1);
 		return;
 	}
-	if(chdir(args[0]) == -1) {
+	if(chdir(args[1]) == -1) {
 		perror("Command failed");
 	}
 }
@@ -49,13 +47,13 @@ void exec_command(char** args, int size) {
 	}
 }
 
-void execute_as_child_process(char *cmd, char **args) {
+void execute_as_child_process(char* cmd, char** args) {
 	pid_t pid = fork();
 	if(pid < 0)
 		perror("Fork failed");
 	if(pid == 0) {
 		if(execv(cmd, args) == -1) {
-			perror("Command failed");
+			perror("Command execution failed");
 		}
 		exit_command(0);
 	}
@@ -63,33 +61,75 @@ void execute_as_child_process(char *cmd, char **args) {
 		perror("waitpid");
 }
 
+// Access file to know if it exists or not
+int access_file(char* filepath) {
+	struct stat sb;
+	if (stat(filepath, &sb) == -1) {
+		return 0;
+	}
+	return 1;
+}
+
+// Adds the filename to the directory path for searching
+void append_filename(char* dirpath, char* filename) {
+	*dirpath = '/';
+	dirpath++;
+	while(*filename != '\0') {
+		*dirpath =  *filename;
+		dirpath++;
+		filename++;
+	}
+	*dirpath = '\0';
+}
+
+/* Searches for file if input does not match any valid commands.
+   If the file file is found then, it is executed with its arguments.*/
+void unrecognized_command(char* cmd, char** args) {
+	int file_found = 0, f = 0,  p = 0;
+	char* dirpath = (char*)malloc(sizeof(char)*4*INPUT_CHARACTERS);
+	char* all_dirs = getenv("PATH");
+	while(all_dirs[p] != '\0') {
+		if (all_dirs[p] == ':') {
+			append_filename(dirpath+f, cmd);
+			if (access_file(dirpath)) {
+				file_found = 1;
+				break;
+			}
+			else {
+				free(dirpath);
+				dirpath = (char*)malloc(sizeof(char)*4*INPUT_CHARACTERS);
+				f = 0;
+				p++;
+			}
+		}
+		else
+			dirpath[f++] = all_dirs[p++];
+	}
+	if (file_found) 
+		execute_as_child_process(dirpath, args);
+	else 
+		printf("Unrecognized command: %s\n", cmd);
+}
+
 int main() {
 	prompt();
 	size_t size = INPUT_CHARACTERS;
-	char* line = (char *)malloc(sizeof(char)*size);
-	if(line == NULL) {
+	char* input = (char *)malloc(sizeof(char)*size);
+	if(input == NULL) {
 		perror("Unable to allocate buffer\n");
 		exit(1);
 	}
 	while(1) {
-		if(getline(&line, &size, stdin) == -1) {
+		if(getline(&input, &size, stdin) == -1) {
+			perror("getline");
 			exit(1);
 		}
-		if(line[0] != '\n') {
-			// Input command
-			int l = 0, i = 0, j = 0;
-			char cmd[INPUT_CHARACTERS];
-			while(i < INPUT_CHARACTERS && line[l] != '\0' && !isspace(line[l])){
-				cmd[i++] = line[l++];
-			}
-			l++;
-			cmd[i] = '\0';
-			// Arguments
-			char** args= (char**)malloc(sizeof(char*)*ARGUMENTS);
+		if(input[0] != '\n') { 
+			int i = 0, l = 0, j = 0;
+			char** args= (char**)malloc(sizeof(char*)*ARGUMENTS); // stores space separated command and its arguments
 			args[0] = (char*)malloc(sizeof(char)*INPUT_CHARACTERS);
-			i = 0;
-			while ((i < ARGUMENTS) && (j < INPUT_CHARACTERS) && (line[l] != '\0')){
-				if (isspace(line[l])) {
+			while ((i < ARGUMENTS) && (j < INPUT_CHARACTERS) && (input[l] != '\0')){
+				if (isspace(input[l])) {
 					args[i][j] = '\0';
 					i++;
 					l++;
@@ -97,28 +137,38 @@ int main() {
 					args[i] = (char*)malloc(sizeof(char)*INPUT_CHARACTERS);
 				}
 				else
-					args[i][j++] = line[l++];
+					args[i][j++] = input[l++];
 			}
 			args[i][j] = '\0';
-			// Matching commands
+			char* cmd = args[0];
+			// if input command is an executable then execute it in a child process
 			if(cmd[0] == '.' || cmd[0] == '/') {
 				execute_as_child_process(cmd, args);
 			}
+			// if input command is exit then exit from the shell
 			else if(strcmp(cmd, "exit") == 0) {
 				exit_command(i);
 			}
+			// if input command is cd then change directory
 			else if(strcmp(cmd, "cd") == 0) {
 				cd_command(args, i);
 			}
+			// if input command is exec then execute the given command
 			else if(strcmp(cmd, "exec") == 0) {
 				exec_command(args, i);
 			}
+			// if command does not matches above then its an unrecognized command
 			else {
-				printf("Unrecognized command: %s\n", cmd);
+				unrecognized_command(cmd, args);
+			}
+			// free up memory
+			while(i >= 0) {
+				free(args[i]);
+				i--;
 			}
 		}
 		prompt();
 	}
-
+	free(input);
 	return 0;
 }
